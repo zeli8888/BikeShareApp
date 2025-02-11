d0446cbef6db09c5f450cf9f6559a1c2a7acd68b
Edwin <lize201@mails.ucas.ac.cn>
2025-02-11 15:50:15 +0000

    lab2 new py file added

---
+++ labs/lab2/db_config.py
@@ -0,0 +1,6 @@
+# Define database connection URL (SQLAlchemy format)
+USER = "admin"  # Your RDS username
+PASSWORD = "as5071565"  # Your RDS password
+DB_URI = "127.0.0.1"  # Localhost because of SSH tunnel
+PORT = "3333"  # Must match your tunnel port
+DB = "dbbikes"  # Change to your database name
