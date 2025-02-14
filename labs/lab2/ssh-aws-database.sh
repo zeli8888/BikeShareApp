84ed1dd89d384ad84ec4e91c950c77cc3c55cfbe
Edwin <lize201@mails.ucas.ac.cn>
2025-02-14 11:43:12 +0000

    new aws set up

--- labs/lab2/ssh-aws-database.sh
+++ labs/lab2/ssh-aws-database.sh
@@ -1,1 +1,1 @@
-ssh -i aws-flask.pem -L 3333:database-download-jc-decaux.cdq06yk88d22.us-east-1.rds.amazonaws.com:3306 ubuntu@ec2-54-146-195-186.compute-1.amazonaws.com
+ssh -i comp30830.pem -L 3333:comp30830.cni206o6w92y.eu-west-1.rds.amazonaws.com:3306 ubuntu@ec2-34-246-160-129.eu-west-1.compute.amazonaws.com
