dd7529fa6c430f5edb5f916db44384fb8bf00960
Edwin <lize201@mails.ucas.ac.cn>
2025-02-19 00:28:44 +0000

    refactor(scraper): bikes scraper refact, columns will update not only insert. weather columns confused, may just follow instructions, some attributes are based on position, so will not work even if stored in database

--- database/bike_scraper.py
+++ database/bike_scraper.py
@@ -4,62 +4,96 @@ from usage import *
 import argparse
 import time
 import simplejson as json
+import datetime
 
 def database_initialization(engine):
     sql = '''
     CREATE TABLE IF NOT EXISTS station (
-    address VARCHAR(256), 
-    banking INTEGER,
-    bike_stands INTEGER,
-    bonus INTEGER,
-    contract_name VARCHAR(256),
-    name VARCHAR(256),
-    number INTEGER,
-    position_lat REAL,
-    position_lng REAL,
-    status VARCHAR(256));
+    number INTEGER NOT NULL, 
+    address VARCHAR(128), 
+    banking INTEGER, 
+    bike_stands INTEGER, 
+    name VARCHAR(128), 
+    position_lat FLOAT, 
+    position_lng FLOAT, 
+    PRIMARY KEY (number)
+    );
     '''
-    res = commit_sql(engine, sql)
+    # bonus INTEGER
+    commit_sql(engine, sql)
     
     sql = """
     CREATE TABLE IF NOT EXISTS availability (
-    number INTEGER,
-    available_bikes INTEGER,
-    available_bike_stands INTEGER,
-    last_update DATETIME
+        number INTEGER NOT NULL,
+        last_update DATETIME NOT NULL,
+        available_bikes INTEGER,
+        available_bike_stands INTEGER,
+        status VARCHAR(128),
+        PRIMARY KEY (number, last_update),
+        FOREIGN KEY (number) REFERENCES station(number)
     );
     """
-    res = commit_sql(engine, sql)
+    commit_sql(engine, sql)
     
 
-def bike_data_scraper(engine):
+def bike_data_scraper(engine, non_static=True):
     r = requests.get(STATIONS_URI, params={"apiKey": JCKEY, "contract": NAME})
     stations = json.loads(r.text)
     for station in stations:
+        if not non_static:
+            vals = {
+                'number': station.get('number'),
+                'address': station.get('address'),
+                'banking': int(station.get('banking')),
+                'bike_stands': int(station.get('bike_stands')),
+                'name': station.get('name'),
+                'position_lat': station.get('position').get('lat'),
+                'position_lng': station.get('position').get('lng')
+            }
+            sql = """
+            INSERT INTO station (number, address, banking, bike_stands, name, position_lat, position_lng)
+            VALUES (:number, :address, :banking, :bike_stands, :name, :position_lat, :position_lng)
+            ON DUPLICATE KEY UPDATE
+                address = VALUES(address),
+                banking = VALUES(banking),
+                bike_stands = VALUES(bike_stands),
+                name = VALUES(name),
+                position_lat = VALUES(position_lat),
+                position_lng = VALUES(position_lng);
+            """
+            commit_sql(engine, sql, vals)
+        
         vals = {
-            'address': station.get('address'),
-            'banking': int(station.get('banking')),
-            'bike_stands': int(station.get('bike_stands')),
-            'name': station.get('name'),
+            'number': station.get('number'),
+            'last_update': datetime.datetime.fromtimestamp(station.get('last_update')/1000),
+            'available_bikes': station.get('available_bikes'),
+            'available_bike_stands': station.get('available_bike_stands'),
             'status': station.get('status')
         }
         sql = """
-        INSERT INTO station (address, banking, bike_stands, name, status) 
-        VALUES (:address, :banking, :bike_stands, :name, :status);
+        INSERT INTO availability (number, last_update, available_bikes, available_bike_stands, status)
+        VALUES (:number, :last_update, :available_bikes, :available_bike_stands, :status)
+        ON DUPLICATE KEY UPDATE
+            available_bikes = VALUES(available_bikes),
+            available_bike_stands = VALUES(available_bike_stands),
+            status = VALUES(status);
         """
         commit_sql(engine, sql, vals)
 
 def main(database="LOCAL", no_echo=True, loop=False, scraper_interval=5*60):
     engine = get_mysql_engine(database, no_echo)
     database_initialization(engine)
-    
+
     try:
-        bike_data_scraper(engine)
-        while loop:
-            time.sleep(scraper_interval)
-            bike_data_scraper(engine)
+        bike_data_scraper(engine, non_static=False)
     except:
         print(traceback.format_exc())
+    while loop:
+        try:
+            time.sleep(scraper_interval)
+            bike_data_scraper(engine)
+        except:
+            print(traceback.format_exc())
         
 if __name__ == "__main__":
     # to run: 
