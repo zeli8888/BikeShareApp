809ded890f9268fd34104149fb4b049a783a01f3
Ubuntu <ubuntu@ip-172-31-17-102.eu-west-1.compute.internal>
2025-04-04 18:23:50 +0000

    feat(ml): ec2 test for ml done

--- machine-learning/for_test_on_ec2.py
+++ machine-learning/for_test_on_ec2.py
@@ -1,23 +1,31 @@
-from datetime import datetime
 import os
+import warnings
+# Set environment variable to ignore CUDA
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  # Set to 3 to hide all logs, warnings, and errors
+from datetime import datetime
+import holidays
 import pandas as pd
 import numpy as np
-import holidays
 from sklearn.metrics import mean_absolute_error, r2_score
-from tensorflow.keras.models import load_model
-import warnings
 import joblib
-# Ignore UserWarnings from Keras
-warnings.filterwarnings("ignore", category=UserWarning, module="keras")
+# Import TensorFlow after setting the environment variable
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+# Ignore UserWarnings from Keras and other warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", module="sklearn.utils.validation")
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=FutureWarning)
 def train_model(station_id, visualize=False):
     # Load the data
-    data = pd.read_csv(f'training_data/station_{station_id}.csv')
+    data = pd.read_csv(os.path.join(os.path.dirname(__file__), f'training_data/station_{station_id}.csv'))
     X = data.drop(['available_bikes', 'available_docks'], axis=1)
-    y = data[['available_bikes', 'available_docks']]
+    y = data[['available_bikes', 'available_docks']].values
 
     # Predict using the model
-    model = load_model(f'trained_model/station_{station_id}_model.keras')
-    scaler = joblib.load(f'trained_model/scaler_station_{station_id}.joblib')
+    model = load_model(os.path.join(os.path.dirname(__file__), f'trained_model/station_{station_id}_model.keras'))
+    scaler = joblib.load(os.path.join(os.path.dirname(__file__), f'trained_model/scaler_station_{station_id}.joblib'))
     X_test = scaler.transform(X)
     y_pred = model.predict(X_test, verbose=visualize)
 
@@ -31,4 +39,6 @@ def train_model(station_id, visualize=False):
     return mae_bikes, r2_score_bikes, mae_docks, r2_score_docks
 
 if __name__ == '__main__':
-    train_model(1)
+    mae_bikes, r2_score_bikes, mae_docks, r2_score_docks = train_model(1)
+    print(f"MAE bikes: {mae_bikes}, R2 score bikes: {r2_score_bikes}, MAE docks: {mae_docks}, R2 score docks: {r2_score_docks}")
+
